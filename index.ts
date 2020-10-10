31140b7ec452c8ba122edec6aa4c3cd2495d83e0
dcastro0803 <37124311+dcastro0803@users.noreply.github.com>
2020-10-10 12:57:50 -0500

    cambio Daniel

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// Mi cambio
 import express from "express";
 import * as mysql from "mysql";
 import { Home } from "./endpoints/home";
@@ -24,38 +25,61 @@ let DATABASE = "test";
 let conn = mysql.createConnection({
     host: "true-sandbox.cokvb4qleob2.us-west-2.rds.amazonaws.com",
     user: "dbtester",
-    password: 'D:Htn&3G%u574GNz',
+    password: "D:Htn&3G%u574GNz",
     database: DATABASE,
-    multipleStatements: true
+    multipleStatements: true,
 });
 
 conn.connect((e) => {
-    if(e) throw e;
+    if (e) throw e;
     console.log("Connected to db " + DATABASE);
-})
+});
 
 app.use(express.static(__dirname + "/html"));
 
 app.use("/api/home", Home(express.Router(), conn));
-app.use("/api/medicines", Medicines(express.Router(), conn, new Telemetry("/medicines")));
-app.use("/api/protocols", Protocols(express.Router(), conn, new Telemetry("/protocols")));
-app.use("/api/breeds", Breeds(express.Router(), conn, new Telemetry("/breeds")));
-app.use("/api/origins", Origins(express.Router(), conn, new Telemetry("/origins")));
-app.use("/api/providers", Providers(express.Router(), conn, new Telemetry("/providers")));
-app.use("/api/corrals", Corrals(express.Router(), conn, new Telemetry("/corrals")));
-app.use("/api/implants", Implants(express.Router(), conn, new Telemetry("/implants")));
+app.use(
+    "/api/medicines",
+    Medicines(express.Router(), conn, new Telemetry("/medicines"))
+);
+app.use(
+    "/api/protocols",
+    Protocols(express.Router(), conn, new Telemetry("/protocols"))
+);
+app.use(
+    "/api/breeds",
+    Breeds(express.Router(), conn, new Telemetry("/breeds"))
+);
+app.use(
+    "/api/origins",
+    Origins(express.Router(), conn, new Telemetry("/origins"))
+);
+app.use(
+    "/api/providers",
+    Providers(express.Router(), conn, new Telemetry("/providers"))
+);
+app.use(
+    "/api/corrals",
+    Corrals(express.Router(), conn, new Telemetry("/corrals"))
+);
+app.use(
+    "/api/implants",
+    Implants(express.Router(), conn, new Telemetry("/implants"))
+);
 app.use("/api/alots", Alots(express.Router(), conn, new Telemetry("/alots")));
-app.use("/api/lorries", Lorries(express.Router(), conn, new Telemetry("/lorries")));
+app.use(
+    "/api/lorries",
+    Lorries(express.Router(), conn, new Telemetry("/lorries"))
+);
 
-app.get('/*', function(req, res) {
-    res.sendFile((__dirname + '/html/index.html'), function(err: any) {
+app.get("/*", function (req, res) {
+    res.sendFile(__dirname + "/html/index.html", function (err: any) {
         if (err) {
-            res.status(500).send(err)
+            res.status(500).send(err);
         }
     });
 });
 
-
 app.listen(8070, () => {
     console.log("Done on port: " + 8070);
-})
+});
