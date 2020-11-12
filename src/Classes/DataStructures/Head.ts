c97a5c54f0a3f28e3cd8e61474513e90dfcd1954
Martin Rivas <A00818919@itesm.mx>
2020-11-11 19:31:15 -0600

    + Added backend comms to SellAlotList

--- src/Classes/DataStructures/Head.ts
+++ src/Classes/DataStructures/Head.ts
@@ -1,10 +1,33 @@
-export interface Head{
-    id?: string,
-    siniga: string,
-    idLocal: string,
-    sex: "male" | "female",
-    idAlot: string
-    alotName: string
-    weight: number
+import { ISexClass } from "./SexClass";
+
+export interface IN_Head{
+    id: string
+    siniga: string
+    idLocal: string
+    status: "ok" | "sold" | "dead"
+    sex: "male" | "female"
+    idAlot: number
+    alotName: string | null
+    idCorral: number
+    corralName: string | null
+    sexClass: ISexClass
+    lastWeight: number
+    idProvider: number
     providerName: string
+}
+
+export interface OUT_Head{
+    siniga: string
+    idLocal: string
+    sex: "male" | "female"
+    idAlot: number
+    sexClass: number
+    weight: number
+}
+
+export interface OUT_PricedHead{
+    idHead: string
+    priceStand: number
+    priceTotal: number
+    finalWeight: number
 }
